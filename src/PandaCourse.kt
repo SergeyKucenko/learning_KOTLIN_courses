bafddc605f6c5ed3eafa148eb6c93259ddef8d7b
Sergey Kucenko <seo-ksn@yandex.ru>
2026-03-06 17:59:51 +0300

    4 day learning

--- src/PandaCourse.kt
+++ src/PandaCourse.kt
@@ -1,7 +1,4 @@
 fun main() {
 
-    var temp = readln()
-
-    println("Температура за бортом: $temp")
 
 }
--- src/SumunCourse.kt
+++ src/SumunCourse.kt
@@ -1,4 +1,18 @@
 fun main() {
 
+    val name = readln()
+    val date = readln()
+    val cityBorn = readln()
+    val father = readln()
+    val mother = readln()
+    val brother = readln()
+    val wife = readln()
+
+
+
+    println("Имя: $name\nДата рождения: $date\nМесто рождения: $cityBorn\nОтец: $father\nМать: $mother\nБрат: $brother\nЖена: $wife")
+
+
+
 
 }
