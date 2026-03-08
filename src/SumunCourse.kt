02b9dc7dc467315d54fe7f96843b664c38bedb37
Sergey Kucenko <seo-ksn@yandex.ru>
2026-03-08 19:49:43 +0300

    6 day learning

--- src/SumunCourse.kt
+++ src/SumunCourse.kt
@@ -1,14 +1,38 @@
 fun main() {
 
-    val seconds = readln().toInt()
-    val hours = seconds / 3600
-    val secondsForMinuts = seconds % 3600
-    val minutes = secondsForMinuts / 60
-    val leftSeconds = secondsForMinuts % 60
+
+    val valuePowerCar = readln().toInt()
 
 
-    println(" $hours часы  : $minutes минуты : $leftSeconds секунды ")
 
 
+    if (valuePowerCar <= 100 ) {
+        println("Вид ТС: легковой автомобиль")
+        println("Мощность двигателя: $valuePowerCar л.с.")
+        println("Налоговая ставка: 10 руб./л.с.")
+        println("Сумма налога: ${valuePowerCar * 10} руб.")
+
+    } else if (valuePowerCar > 100 && valuePowerCar <= 150) {
+        println("Вид ТС: легковой автомобиль")
+        println("Мощность двигателя: $valuePowerCar л.с.")
+        println("Налоговая ставка: 34 руб./л.с.")
+        println("Сумма налога: ${valuePowerCar * 34} руб.")
+    } else if (valuePowerCar > 150 && valuePowerCar <= 200) {
+        println("Вид ТС: легковой автомобиль")
+        println("Мощность двигателя: $valuePowerCar л.с.")
+        println("Налоговая ставка: 49 руб./л.с.")
+        println("Сумма налога: ${valuePowerCar * 49} руб.")
+    } else if (valuePowerCar > 200 && valuePowerCar <= 250) {
+        println("Вид ТС: легковой автомобиль")
+        println("Мощность двигателя: $valuePowerCar л.с.")
+        println("Налоговая ставка: 75 руб./л.с.")
+        println("Сумма налога: ${valuePowerCar * 75} руб.")
+    } else if (valuePowerCar >= 250) {
+        println("Вид ТС: легковой автомобиль")
+        println("Мощность двигателя: $valuePowerCar л.с.")
+        println("Налоговая ставка: 150 руб./л.с.")
+        println("Сумма налога: ${valuePowerCar * 150} руб.")
+    }
 
 }
+
